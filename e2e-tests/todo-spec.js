1ff6d155ecd66f155d7499141a6a58213def0d12
charitha33 <charithaa33@gmail.com>
2018-08-27 10:07:08 -0500

    change in input

---
+++ e2e-tests/todo-spec.js
@@ -0,0 +1,7 @@
+describe('angularjs homepage todo list', function() {
+    it('should add a todo', function() {
+        browser.get('http://localhost:63342/project%20final/app/index.html?_ijt=h56fnsrvvul0v5makj5kruco2c');
+
+
+    });
+});
